497f46be8073674c647d04cf2ce6eedc40af1ead
phung-an-giang <gphung492@gmail.com>
2024-11-16 18:32:43 +0700

    Add files via upload

---
+++ buoi4.java
@@ -0,0 +1,16 @@
+
+public class Buoi4 {
+    public static void main(String[] args) {
+        // Khai báo và khởi tạo mảng
+        int arr[] = {3, 5, 8, 10, 14, 25};
+        
+        // Hiển thị các phần tử của mảng
+        System.out.println("Các phần tử trong mảng là:");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println("Phần tử tại vị trí " + i + " là: " + arr[i]);
+        }
+    }
+
+{
+    
+}
